bd83517d1eb8e8f28e9f5020d40d7754c4ac49f0
gwanggyu park <pgg961@gmail.com>
2021-01-29 21:11:04 +0900

    solve error

--- src/App.js
+++ src/App.js
@@ -1,39 +1,47 @@
 import React from "react";
 import axios from "axios";
-import Movie from "./movies";
+import Movie from "./Movie";
 
-class App extends React.Component{
+class App extends React.Component {
   state = {
-    isLoading : true, 
+    isLoading: true,
     movies: []
   };
 
-  getMovies = async() => {
+  getMovies = async () => {
     const {
-      data :{
-        data :{movies}
+      data: {
+        data: { movies }
       }
-    } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
+    } = await axios.get(
+      "https://yts.mx/api/v2/list_movies.json?sort_by=rating"
+      );
     console.log(movies);
-    this.setState({movies, isLoading : false});
-  }
+    this.setState({ movies, isLoading: false });
+  };
 
-  componentDidMount(){
+  componentDidMount() {
     this.getMovies();
   }
 
-  render(){
-    const {isLoading, movies} = this.state;
-    return isLoading? "Loading..." : 
-      <Movie 
-        id={movie.id} 
-        year={movie.year} 
-        title={movie.title} 
-        summary={movie.summary} 
-        poster={movie.medium_cover_movie} />
-    
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+    <div> 
+      {isLoading ? "Loading..." : movies.map(movie => (
+      // console.log(movie);
+      <Movie
+        key={movie.id}
+        id={movie.id}
+        year={movie.year}
+        title={movie.title}
+        summary={movie.summary}
+        poster={movie.medium_cover_image}
+      />
+      ))}
+    </div>
+    );
   }
-
 }
 
 export default App;

a661fb885c80264067972c7f5969a3803b707aab
gwanggyu park <pgg961@gmail.com>
2021-01-29 19:31:34 +0900

    working on movie_app

--- src/App.js
+++ src/App.js
@@ -1,71 +1,39 @@
 import React from "react";
-import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "./movies";
 
-function Food({name, picture, rating}){
+class App extends React.Component{
+  state = {
+    isLoading : true, 
+    movies: []
+  };
 
-  return <div>
-          <h2>I like {name}</h2>
-          <h4>{rating}/5.0</h4>
-          <img src ={picture}/>
-        </div>;
-
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number
-}
-
-const foodILike = [
-  {
-    id : 1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rating : 5
-  },
-  { 
-    id: 2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-  
-  },
-  {
-    id : 3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-    rating: 4.8
-    },
-  {
-    id: 4, 
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating: 4.7
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    rating: 4.6
+  getMovies = async() => {
+    const {
+      data :{
+        data :{movies}
+      }
+    } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
+    console.log(movies);
+    this.setState({movies, isLoading : false});
   }
-];
 
-// function renderFood(dish){
-//   console.log(dish);
-//   return <Food name = {dish.name} picture = {dish.image}/>
-// }
+  componentDidMount(){
+    this.getMovies();
+  }
 
-function App() {
-  return (<div>
-           {foodILike.map(dish =>
-              <Food key = {dish.id} name = {dish.name} picture = {dish.image} rating = {dish.rating} />
-            )}
-          </div>);
+  render(){
+    const {isLoading, movies} = this.state;
+    return isLoading? "Loading..." : 
+      <Movie 
+        id={movie.id} 
+        year={movie.year} 
+        title={movie.title} 
+        summary={movie.summary} 
+        poster={movie.medium_cover_movie} />
+    
+  }
+
 }
 
 export default App;
